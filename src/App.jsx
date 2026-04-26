1e2869441396f371bcfac5df3e23ed280114006f
Dream <gracehoong@gmail.com>
2026-04-26 22:48:07 +0800

    fix drawdown page component name

--- src/App.jsx
+++ src/App.jsx
@@ -2325,7 +2325,7 @@ export default function App() {
   else if (path === "/disclaimer") page = <DisclaimerPage />;
   else if (path === "/contact") page = <ContactPage />;
   else if (path === "/forex-market-hours") page = <ForexMarketHoursPage />;
-  else if (path === "/drawdown-calculator") page = <DrawdownCalculatorPage />;
+  else if (path === "/drawdown-calculator") page = <RiskRewardCalculatorPage />;
   else if (path === "/articles") page = <ArticlesPage />;
   else if (path === "/articles/what-is-risk-per-trade-in-forex") page = <RiskPerTradeArticlePage />;
   else if (path === "/articles/conservative-ea-settings-for-small-accounts") page = <ConservativeEASettingsArticlePage />;
